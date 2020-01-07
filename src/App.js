7342d8c22ad2fe4ee83d1f1cbfaf493850861860
Rockstar <faras.m@quickbillsmd.com>
2020-01-07 11:12:04 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "./components/Header/Header";
+
+import AsideMenu from "./components/AsideMenu/AsideMenu";
+
+function App() {
+  return (
+    <React.Fragment>
+      <AsideMenu />
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;
